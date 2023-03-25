1791a46e12af3ee57c8b8118494ff9b920f8305e
Artem <shurtemm@mail.ru>
2023-03-25 03:04:02 +0300

    Решили задачу 50

---
+++ Task50/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
+и возвращает позицию этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [rows, cols];
+Console.WriteLine("");
+
+FillArray(array);
+PrintArray(array);
+FindNum(array);
+
+
+
+void FillArray(int[,] array)
+    {
+    for (int i=0; i<array.GetLength(0);i++)
+        for (int j=0; j<array.GetLength(1);j++)
+            array[i,j] = new Random().Next(-3,3);
+    }
+
+void PrintArray(int[,] array)
+    {
+    for (int i=0; i<array.GetLength(0);i++)
+        {
+        for (int j=0; j<array.GetLength(1);j++)
+            Console.Write($"{array[i,j],3} \t");
+        Console.WriteLine();
+        }
+    }
+
+void FindNum(int[,] array)
+    {
+    int k = 0;
+    Console.WriteLine();
+    Console.WriteLine("Введите искомое число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Позиции искомого числа:");
+    for (int i=0; i<array.GetLength(0);i++)
+        for (int j=0; j<array.GetLength(1);j++)
+            {
+            if (array[i,j] == num)
+                {
+                Console.WriteLine($"[{i},{j}]");
+                k++;
+                }
+            }
+    if (k==0)
+    Console.WriteLine("Такого числа в массиве нет");
+    }
