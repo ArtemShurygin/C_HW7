dd7c9e87acc72fec50f33407e409b9514261ac4f
Artem <shurtemm@mail.ru>
2023-03-25 17:28:58 +0300

    Изменили диапазон случайных чисел в задаче 50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -26,7 +26,7 @@ void FillArray(int[,] array)
     {
     for (int i=0; i<array.GetLength(0);i++)
         for (int j=0; j<array.GetLength(1);j++)
-            array[i,j] = new Random().Next(-3,3);
+            array[i,j] = new Random().Next(-10,10);
     }
 
 void PrintArray(int[,] array)
